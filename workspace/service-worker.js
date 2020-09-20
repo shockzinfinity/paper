ed16aaa2b6379bdd96e63ace9cd3f59616a7940e
Jun Yu <shockz@me.com>
2020-09-20 15:44:07 +0900

    fetch 를 통한 조작

--- workspace/service-worker.js
+++ workspace/service-worker.js
@@ -8,4 +8,10 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   console.log('Service worker - fetch', event.request.url);
+
+  // .jpg 확장자 파일을 요청할 경우 모두 강아지 사진으로 대체
+  if (event.request.url.endsWith('.jpg')) {
+    console.log('멍멍');
+    event.respondWith(fetch('/upload/puppy.jpg'));
+  }
 });
